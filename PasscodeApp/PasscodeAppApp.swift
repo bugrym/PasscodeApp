cbcc20e1f55ac37bdf638674aff4bb934b92a06b
vbugrym <vbugrym@lohika.com>
2024-03-05 22:23:41 +0200

    Initial Commit

---
+++ PasscodeApp/PasscodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasscodeAppApp.swift
+//  PasscodeApp
+//
+//  Created by vbugrym on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PasscodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
